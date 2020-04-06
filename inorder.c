02cc3e8c668798181bc7ae4fa160a112983212e4
PraNjaL-16 <54860381+PraNjaL-16@users.noreply.github.com>
2020-04-06 19:38:53 +0530

    Add files via upload

---
+++ inorder.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    struct node * lchild;
+    int data;
+    struct node * rchild;
+}*root=NULL;
+
+void inorder(struct node * p)
+{
+    if(p)
+    {
+        inorder(p->lchild);
+        printf("%d\t",p->data);
+        inorder(p->rchild);
+    }
+}
+
+struct node * Rcreate()
+{
+    int x;
+    static int flag = 0;
+    struct node * t;
+
+    if(flag == 0)
+    {
+        printf("enter node data : ");
+        flag ++;
+    }
+    scanf("%d",&x);
+
+    if(x == -1)
+    {
+        return NULL;
+    }
+
+    t = (struct node *)malloc(sizeof(struct node));
+    t->data = x;
+    t->lchild = t->rchild = NULL;
+
+    printf("enter left child of %d : ",t->data);
+    t->lchild = Rcreate();
+
+    printf("enter right child of %d : ",t->data);
+    t->rchild = Rcreate();
+
+    return t;
+}
+
+int main()
+{
+    root = Rcreate();
+
+    inorder(root);
+
+    return 0;
+}
